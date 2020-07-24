6e6c12bd3ea6d022e788c423560a6ec34580589b
javier-esquivel <50594068+javier-esquivel@users.noreply.github.com>
2020-07-23 19:43:33 -0600

    1

---
+++ test.js
@@ -0,0 +1,6 @@
+janrain.settings.capture = janrain.settings.capture || {}
+janrain.settings.capture.beforeJanrainCaptureWidgetOnLoad = janrain.settings.capture.beforeJanrainCaptureWidgetOnLoad || []
+janrain.settings.capture.beforeJanrainCaptureWidgetOnLoad.push(function() {
+    // Insert customer JS here
+    connsole.log("test");
+})
